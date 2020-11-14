da9703285e7c8530f0d7adadf5d8cf668db52f48
leopedrassoli <leona@DESKTOP-4DBML2U>
2020-11-14 15:49:59 -0200

    Segundo commit

--- src/projmosquitto/ProjMosquitto.java
+++ src/projmosquitto/ProjMosquitto.java
@@ -4,8 +4,14 @@ package projmosquitto;
  *
  * @author Leonardo Pedrassoli Silva 11521ECP003
  */
+import entidades.*;
+
 public class ProjMosquitto {
     public static void main(String[] args) {
+        
+        MqttPublishSample publ = new MqttPublishSample();
+        
+        //publ.
         
     }
     
